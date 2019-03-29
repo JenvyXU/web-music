7eba7bd0bb7845e9df0604e13166dce67f18305e
xzw <firetcandy@gmail.com>
2019-03-29 23:31:35 +0800

    添加列表切换功能

--- main.js
+++ main.js
@@ -2,24 +2,13 @@ var currentIndex = 0
 var audio = new Audio()
 var clock
 var musicList=[]
-audio.autoplay = true
 
 
-// audio.ontimeupdate = function () {
-//   console.log(this.currentTime)
-//   var playMin = Math.floor(this.currentTime / 60) + ''
-//   var playSec = Math.floor(this.currentTime) % 60 + ''
-//   playMin = playMin.length === 2 ? playMin : '0' + playMin
-//   playSec = playSec.length === 2 ? playSec : '0' + playSec
-
-//   var totalMin = Math.floor(this.duration / 60) + ''
-//   var totalSec = Math.floor(this.duration) % 60 + ''
-//   totalMin = totalMin.length === 2 ? totalMin : '0' + totalMin
-//   totalSec = totalSec.length === 2 ? totalSec : '0' + totalSec
-//   $('.bar>.bar-progress').style.width = (this.currentTime / this.duration) * 100 + '%'
-//   $('.play-time>:nth-child(1)').innerText = playMin + ':' + playSec
-//   $('.play-time>:nth-child(2)').innerText = totalMin + ':' + totalSec
-// }
+getMusicList(function (list) {
+  musicList=list
+  generateList(list)
+  loadMusic(list[currentIndex])
+})
 
 audio.onplay=function(){
   clock=setInterval(()=>{
@@ -36,18 +25,23 @@ audio.onplay=function(){
     $('.play-time>:nth-child(1)').innerText = playMin + ':' + playSec
     $('.play-time>:nth-child(2)').innerText = totalMin + ':' + totalSec
   },1000)
+  isPlaying()
 }
 
 audio.onpause=function(){
   clearInterval(clock)
 }
+
 audio.onended=function(){
   currentIndex=(++currentIndex)%musicList.length
-  loadMusic(musicList[currentIndex])
+  setTimeout(() => {
+    loadMusic(musicList[currentIndex])
+    audio.play()   
+  }, 3000);
+
 }
 
 $('.control .play').onclick=function(){
-  
   if(audio.paused){
     audio.play()  
     this.querySelector('.play-btn').classList.remove('active')
@@ -57,17 +51,22 @@ $('.control .play').onclick=function(){
     this.querySelector('.pause-btn').classList.remove('active')
     this.querySelector('.play-btn').classList.add('active')
   }
-
 }
 
 $('.control .pre').onclick=function(){
+  document.querySelectorAll('.play-list>ul>li')[currentIndex].classList.remove('active')
   currentIndex=(musicList.length+(--currentIndex))%musicList.length
+  document.querySelectorAll('.play-list>ul>li')[currentIndex].classList.add('active')
   loadMusic(musicList[currentIndex])
+  audio.play()
 }
 
 $('.control .next').onclick=function(){
+  document.querySelectorAll('.play-list>ul>li')[currentIndex].classList.remove('active')
   currentIndex=(++currentIndex)%musicList.length
+  document.querySelectorAll('.play-list>ul>li')[currentIndex].classList.add('active')
   loadMusic(musicList[currentIndex])
+  audio.play()
 }
 
 $('.progress .bar').onclick=function(e){
@@ -75,6 +74,28 @@ var percent=e.offsetX/parseInt(getComputedStyle(this).width)
 audio.currentTime=audio.duration*percent
 }
 
+$('.play-list>ul').addEventListener('click',function(e){
+  var fatherNode
+  var index
+  if(e.target.nodeName.toLowerCase()==='li'){
+    fatherNode=e.target
+  }else{
+    fatherNode=e.target.parentNode
+    while(fatherNode.nodeName.toLowerCase()!=='li'){
+      fatherNode=fatherNode.parentNode
+    }
+  }
+  index=fatherNode.getAttribute('data-index')
+  for(var key in fatherNode.parentNode.children){
+    if(fatherNode.parentNode.children[key].className==='active'){
+       fatherNode.parentNode.children[key].classList.remove('active')
+   }
+}
+  fatherNode.classList.add('active') 
+  currentIndex=index
+  loadMusic(musicList[currentIndex])
+  audio.play()
+},false)
 
 
 function $(selector) {
@@ -97,16 +118,11 @@ function getMusicList(callback) {
   }
 }
 
-getMusicList(function (list) {
-  musicList=list
-  generateList(list)
-  loadMusic(list[currentIndex])
-})
-
 function loadMusic(musicObj) {
   $('.bar-title>.title>:nth-child(1)').innerText = musicObj.title
   $('.bar-title>.title>:nth-child(2)').innerText = '-' + musicObj.author
   $('.singer-pic>img').setAttribute('src',musicObj.cover)
+  $('.progress>img').setAttribute('src',musicObj.cover)
   audio.src = musicObj.src
 }
 
@@ -120,12 +136,39 @@ function generateList(list){
       <div class="auther-wrapper"><span class="author">${song.author}</span><span class="time">${song.duration}</span></div>
     </div>`
     var songLi=document.createElement('li')
-    if(index===0){
+    if(index===currentIndex){
       songLi.classList.add('active')
     }
+    songLi.setAttribute('data-index',index)
     songLi.innerHTML=tpl
     songList.appendChild(songLi)
   })
   $('.play-list>ul').appendChild(songList)
   $('.play-list>p span').innerText=list.length
 }
+
+function isPlaying(){
+  if(audio.paused){
+    $('.play-btn').classList.add('active')
+    $('.pause-btn').classList.remove('active')
+  }else{
+    $('.pause-btn').classList.add('active')
+    $('.play-btn').classList.remove('active')
+  }
+}
+
+// audio.ontimeupdate = function () {
+//   console.log(this.currentTime)
+//   var playMin = Math.floor(this.currentTime / 60) + ''
+//   var playSec = Math.floor(this.currentTime) % 60 + ''
+//   playMin = playMin.length === 2 ? playMin : '0' + playMin
+//   playSec = playSec.length === 2 ? playSec : '0' + playSec
+
+//   var totalMin = Math.floor(this.duration / 60) + ''
+//   var totalSec = Math.floor(this.duration) % 60 + ''
+//   totalMin = totalMin.length === 2 ? totalMin : '0' + totalMin
+//   totalSec = totalSec.length === 2 ? totalSec : '0' + totalSec
+//   $('.bar>.bar-progress').style.width = (this.currentTime / this.duration) * 100 + '%'
+//   $('.play-time>:nth-child(1)').innerText = playMin + ':' + playSec
+//   $('.play-time>:nth-child(2)').innerText = totalMin + ':' + totalSec
+// }
